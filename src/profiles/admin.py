22bd30151d079ae7a34f3c1ae433a8abdb076233
ali-hv <alihv5000@gmail.com>
2023-12-26 00:27:38 +0330

    add get_full_name to list_display

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -4,4 +4,4 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "first_name", "last_name")
+    list_display = ("user", "get_full_name", )
