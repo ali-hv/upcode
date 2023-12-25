8d74e9356cbc6d2918bcbada67f9b935bf7ca4c8
ali-hv <alihv5000@gmail.com>
2023-12-26 00:06:42 +0330

    register user model to admin

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = (
+        "username",
+        "email",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "is_superuser",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("username", "email", "phone_number", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_superuser",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                    "date_joined",
+                    "last_login",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "phone_number",
+                    "password1",
+                    "password2",
+                    "is_superuser",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                    "date_joined",
+                    "last_login",
+                ),
+            },
+        ),
+    )
+    search_fields = (
+        "email",
+        "username",
+    )
+    ordering = (
+        "is_superuser",
+        "is_staff",
+        "date_joined",
+    )
