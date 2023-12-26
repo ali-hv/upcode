555ee4e36aa39428b6890c4d6fafe775b0b83d9a
ali-hv <alihv5000@gmail.com>
2023-12-26 18:50:00 +0330

    create Contest model

--- src/contests/models.py
+++ src/contests/models.py
@@ -1,3 +1,29 @@
+from django.utils import timezone
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class Contest(models.Model):
+    title = models.CharField(max_length=255)
+    detail = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    registration_start_time = models.DateTimeField()
+    registration_end_time = models.DateTimeField()
+    is_active = models.BooleanField(default=False)
+    participants = models.ManyToManyField(
+        User, related_name="contests_participated", blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    def is_registration_open(self):
+        now = timezone.now()
+        return self.registration_start_time <= now <= self.registration_end_time
+
+    def is_contest_active(self):
+        now = timezone.now()
+        return self.start_time <= now <= self.end_time
