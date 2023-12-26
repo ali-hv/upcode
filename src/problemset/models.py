2fffaa356724725787c44489ad8d94e65aac2c8b
ali-hv <alihv5000@gmail.com>
2023-12-26 17:53:26 +0330

    create Problem, Category and Language models

--- src/problemset/models.py
+++ src/problemset/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Problem(models.Model):
+    title = models.CharField(max_length=255)
+    detail = models.TextField()
+    categories = models.ManyToManyField(Category, related_name="problems")
+    languages = models.ManyToManyField(Language, related_name="problems")
+    time_limit = models.PositiveIntegerField()
+    memory_limit = models.PositiveIntegerField()
+    test_cases = models.FileField(upload_to="test_cases/")
+    # contest = models.ForeignKey(Contest, blank=True)
