aa33772e586fdfb0f6b0dc82841aea4cbc3d31a0
ali-hv <alihv5000@gmail.com>
2023-12-26 17:54:12 +0330

    register Problem, Category and Language models to admin

--- src/problemset/admin.py
+++ src/problemset/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Problem, Category, Language
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = (
+        "categories",
+        "languages",
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ("name",)
