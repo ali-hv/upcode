b630bb4e7dc5ae8f20ca39117c37d01c2aafa485
ali-hv <alihv5000@gmail.com>
2023-12-26 20:09:27 +0330

    register Contest model to admin

--- src/contests/admin.py
+++ src/contests/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contest
+
+
+@admin.register(Contest)
+class ContestAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "start_time",
+        "is_contest_active",
+        "is_registration_open",
+    )
+    list_filter = ("is_active",)
