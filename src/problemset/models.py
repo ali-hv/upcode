0f2c633ba93e632ecc4366ab5430210f81643359
ali-hv <alihv5000@gmail.com>
2023-12-28 01:00:31 +0330

    add level field to Problem model

--- src/problemset/models.py
+++ src/problemset/models.py
@@ -18,14 +18,27 @@ class Language(models.Model):
 
 
 class Problem(models.Model):
+    LEVEL_CHOICES = (
+        ("easy", "ساده"),
+        ("intermediate", "متوسط"),
+        ("hard", "سخت"),
+    )
+
     title = models.CharField(max_length=255)
     detail = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="problems")
     languages = models.ManyToManyField(Language, related_name="problems")
     time_limit = models.PositiveIntegerField()
     memory_limit = models.PositiveIntegerField()
+    level = models.CharField(max_length=12, choices=LEVEL_CHOICES)
     test_cases = models.FileField(upload_to="test_cases/")
-    contest = models.ForeignKey(Contest, blank=True, null=True, on_delete=models.PROTECT, related_name="problems")
+    contest = models.ForeignKey(
+        Contest,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
+        related_name="problems",
+    )
 
     def __str__(self):
         return self.title
