5b3e1425ccb1eba4aea15c3d4804c74bca1a30db
ali-hv <alihv5000@gmail.com>
2023-12-27 20:04:00 +0330

    send tags and contests to problemset page

--- src/problemset/views.py
+++ src/problemset/views.py
@@ -1,5 +1,17 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from contests.models import Contest
+from .models import Problem, Tag
 
 
-class Problemset(TemplateView):
+class Problemset(ListView):
+    model = Problem
+    queryset = model.objects.all()
+    context_object_name = "problemset"
     template_name = "problemset/problemset.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        context["contests"] = Contest.objects.all()
+        return context
