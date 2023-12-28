51ab4dfc38fcc9f140c8644e8f5321825b6a68a2
ali-hv <alihv5000@gmail.com>
2023-12-28 18:27:56 +0330

    register UserProblem to admin

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, UserProblem
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "get_full_name", )
+    list_display = (
+        "user",
+        "get_full_name",
+    )
+
+
+@admin.register(UserProblem)
+class UserProblemAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "problem",
+        "status",
+    )
