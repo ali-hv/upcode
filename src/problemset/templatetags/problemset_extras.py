07bb32ad844566ca0d1afa5ece5206eb8b9b0b68
ali-hv <alihv5000@gmail.com>
2023-12-31 01:05:39 +0330

    create a tag to filter user submissions

--- src/problemset/templatetags/problemset_extras.py
+++ src/problemset/templatetags/problemset_extras.py
@@ -4,18 +4,23 @@ from django.template.defaulttags import register
 @register.filter
 def is_solved(problem, user):
     for i in problem:
-        if i.user == user:
-            if i.status == "solved":
-                return True
-    else:
-        return False
+        if i.user == user and i.status == "solved":
+            return True
+    return False
 
 
 @register.filter
 def is_partial(problems, user):
     for i in problems:
+        if i.user == user and i.status == "partial":
+            return True
+    return False
+
+
+@register.filter
+def get_submissions(problems, user):
+    submissions = []
+    for i in problems:
         if i.user == user:
-            if i.status == "partial":
-                return True
-    else:
-        return False
+            submissions.append(i)
+    return submissions
