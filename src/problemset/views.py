1cbeabdf99897d9c358542f4ab0a16db201f93a4
ali-hv <alihv5000@gmail.com>
2024-01-04 16:10:18 +0330

    get currently selected language and send it with the form

--- src/problemset/views.py
+++ src/problemset/views.py
@@ -1,10 +1,10 @@
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.views import View
 
 from contests.models import Contest
-from .models import Problem, Tag, Submission
+from .models import Problem, Tag, Submission, Language
 
 
 class Problemset(ListView):
@@ -29,11 +29,14 @@ class ProblemPage(DetailView):
 
 class CreateSubmission(View):
     def post(self, request):
+        language_name = request.POST.get("language")
+        language_id = get_object_or_404(Language, name=language_name).id
+
         obj = Submission(
             user=request.user,
             file=request.FILES["submission-file"],
             problem_id=request.POST.get("problem"),
-            language_id=request.POST.get("language"),
+            language_id=language_id,
         )
         obj.save()
 
