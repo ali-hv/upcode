5c988ecec76e770848c579eaadd3469237bbccb2
ali-hv <alihv5000@gmail.com>
2024-02-03 18:31:53 +0330

    create Contests view

--- src/contests/views.py
+++ src/contests/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from contests.models import Contest
+
+
+class Contests(ListView):
+    model = Contest
+    context_object_name = "contests"
+    template_name = "contests/contests.html"
+    paginate_by = 15
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["running_contests"] = self.model.objects.filter(is_active=True)
+        context["done_contests"] = self.model.objects.filter(is_active=False)
+        return context
