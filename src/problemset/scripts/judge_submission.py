f7c91de3553213bfb9af2dce6f2940b01715d9fc
ali-hv <alihv5000@gmail.com>
2024-02-11 17:55:32 +0330

    Refactor: update the judge system to read testcases inputs and outputs from separate files

--- src/problemset/scripts/judge_submission.py
+++ src/problemset/scripts/judge_submission.py
@@ -1,50 +1,60 @@
-from subprocess import Popen, PIPE, STDOUT
-import sys
+from subprocess import Popen, PIPE, TimeoutExpired
 import os
 
 from django.core.files import File
 
 
 def judge_python(submission):
-    with open(submission.problem.test_cases.path, 'r') as f:
-        test_cases = f.readlines()
-    test_cases = [i.strip().split(',') for i in test_cases]
-
-    result_filename = f'{submission.id}-{submission.user_id}-result.txt'
-    result = open(result_filename, 'w').close()
-
-    user_file = submission.file.path
-
     score = 0
 
-    for i in test_cases:
-        _input = i[0]
-    
-        communicate_argument = _input
-        p = Popen(
-            [sys.executable, user_file],
-            stdout=PIPE,
+    result_filename = f"{submission.id}-{submission.user_id}-result.txt"
+    result = open(result_filename, "w").close()
+
+    for test_case in submission.problem.test_cases.all():
+        # Read input from file
+        with open(test_case.input_file.path, "r") as f:
+            input_content = f.read().strip().split("\n")
+
+        user_file = submission.file.path
+
+        # Run the main program and capture output
+        process = Popen(
+            ["python", user_file],
             stdin=PIPE,
-            stderr=STDOUT,
-            encoding="utf-8",
+            stdout=PIPE,
+            stderr=PIPE,
+            text=True,
         )
-    
-        stdout, stderr = p.communicate(communicate_argument)
-    
+
+        memory_limit = submission.problem.memory_limit
+
+        # TODO resource lib only works in unix based operating systems(install in the docker)
+        # resource.setrlimit(resource.RLIMIT_AS, (memory_limit, memory_limit))
+
+        # Read expected output from output1.txt
+        with open(test_case.output_file.path, "r") as f:
+            expected_output = f.read().strip().split("\n")
+
         try:
-            if stdout.strip() == i[1]:
+            output, _ = process.communicate("\n".join(input_content), timeout=submission.problem.time_limit)
+
+            # Compare output with expected output
+            if output.strip().split("\n") == expected_output:
                 with open(result_filename, 'a') as f:
                     f.write("Accepted\n")
                 score += 1
             else:
                 with open(result_filename, 'a') as f:
                     f.write("Wrong Answer\n")
-        except RuntimeError:
+        except TimeoutExpired:
             with open(result_filename, 'a') as f:
-                f.write("Runtime Error\n")
+                f.write("Time Limit Exceeded\n")
+        except MemoryError:
+            with open(result_filename, "a") as f:
+                f.write("Memory Limit Exceeded\n")
 
-        submission.score = score / len(test_cases) * 100
-        submission.save()
+    submission.score = score / len(submission.problem.test_cases.all()) * 100
+    submission.save()
 
     with open(result_filename, 'rb') as file:
         file = File(file)
