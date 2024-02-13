0255546d7a9e805c3dddae2e2487979d5a851ac7
alihv <alihv5000@gmail.com>
2024-02-13 18:48:39 +0330

    Fix: remove an additional method from User model

--- src/users/models.py
+++ src/users/models.py
@@ -14,8 +14,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
-    def get_full_name(self):
-        if self.first_name or self.last_name:
-            return f"{self.first_name} {self.last_name}"
-        return self.username
